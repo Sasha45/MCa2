b1a0114553c32586178e9eb3f0af7a8a535cb9f9
Sasha45 <sasha.kubichka@gmail.com>
2019-08-19 17:37:08 -0700

    Initial commit

---
+++ app/src/main/java/com/example/mca2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mca2;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
